a06353a473e80b05e24e0c4843811489923b56a3
Hector <hectoratiliotrujillo@gmail.com>
2017-08-27 17:11:20 -0400

    added timer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  countdown:string = '';
+  ngOnInit() {
+    this.getCountdown();
+  }
+  getCountdown() {
+    let dateDif = Math.ceil((new Date("2017-11-25").getTime() - new Date().getTime())/ (1000*60*60*24));
+    console.log(dateDif);
+    if(dateDif > 0) 
+      this.countdown = dateDif + ' Days Left!';
+    else if (dateDif == 0)
+      this.countdown = 'It\'s our wedding day!';
+  }
+}
